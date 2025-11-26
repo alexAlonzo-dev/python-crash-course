738aa6693c1aaeb36d6c941be776676fb0bcb478
alexAlonzo-dev <alejandroramirez.dev@outlook.com>
2025-11-25 21:17:14 -0600

    Implements an example about for loop

---
+++ section_4/magicians.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()} Your Cock Magic is Great")
+
+print("\nThank you, everyone. That was a great magic show!")
