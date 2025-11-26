3d0c0476863b3ce533cdbfe0017288483d8a4774
alexAlonzo-dev <alejandroramirez.dev@outlook.com>
2025-11-25 21:22:31 -0600

    Implements some practice exercises

---
+++ section_4/4-1.py
@@ -0,0 +1,6 @@
+pizzas = ["Pepperoni", "Margherita", "Hawaiian", "Meat Lover's"]
+
+for pizza in pizzas:
+    print(f"Just a pizza: {pizza}")
+
+print("I'm not a pizza's big fan")
---
+++ section_4/4-2.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'coyote', 'wolf']
+
+for animal in animals:
+    print(f"Just an animal: {animal}")
+
+print("they belong to the same family 'Canidae'")
